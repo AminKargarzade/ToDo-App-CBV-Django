950b34cadf20592de96955ff2934f00b28c86332
Amin <aminkargarzadeh26@gmail.com>
2026-05-14 18:38:44 +0330

    Writing a test for creating a task!
    What if we want to test the creating a task function without authorization!

--- core/todo/tests/test_task_api.py
+++ core/todo/tests/test_task_api.py
@@ -1,12 +1,23 @@
 from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
+from datetime import datetime
 
 @pytest.mark.django_db
 class TestTaskApi:
+    client = APIClient()
     
     def test_get_task_response_200_status(self):
-        client = APIClient()
         url = reverse("todo:api-v1:task-list")
-        response = client.get(url)
+        response = self.client.get(url)
         assert response.status_code == 200 # type: ignore
+        
+    def test_create_task_response_401_status(self):
+        url = reverse("todo:api-v1:task-list")
+        data = {
+            "title" : "test",
+            "complete" : True,
+            "created_date" : datetime.now()
+        }
+        response = self.client.post(url, data)
+        assert response.status_code == 401  # type: ignore
